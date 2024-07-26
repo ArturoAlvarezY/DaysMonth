49cd4bd0302f40f054f29be75aad3418e6b1aeff
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-26 12:46:54 +0200

    add the app java, finished exercise!

--- src/main/java/dev/arturo/App.java
+++ src/main/java/dev/arturo/App.java
@@ -1,5 +1,7 @@
 package dev.arturo;
 
+import dev.arturo.models.MonthNumber;
+
 /**
  * Hello world!
  */
@@ -12,6 +14,9 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MonthNumber monthNumber = new MonthNumber();
+
+        System.out.println(monthNumber.daysAndMonth(13));
+        System.out.println(monthNumber.daysAndMonth(3));
     }
 }
