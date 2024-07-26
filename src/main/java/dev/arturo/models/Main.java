a8cc7677790ce326cc136059568f0cec41eec56c
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-26 11:19:44 +0200

    addig hamscret and tests files

--- src/main/java/dev/arturo/models/Main.java
+++ src/main/java/dev/arturo/models/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         System.out.println(userInput.getMonthName());
 
-        System.out.println(userInput.getDays());
+        System.out.println(userInput.getMonthName());
      
 }
 }
---
+++ src/test/java/UserInputTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import dev.arturo.models.UserInput;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class UserInputTest {
+
+    UserInput userInput = new UserInput(null, null);
+    @Test
+    void testObtainMonth() {
+        
+
+
+    }
+}
